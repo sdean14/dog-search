e69bce9996eb7a177aafac1aa7baab4216da53e6
satomi <satomiampuku2@gmail.com>
2021-07-20 18:40:45 -0700

    random dog imgs on result page

--- dog-search/component/result_page.jsx
+++ dog-search/component/result_page.jsx
@@ -3,19 +3,35 @@ import { Link, Redirect } from 'react-router-dom';
 import { Home } from './home';
 
 export const Result = (props) => {
-console.log(props.location.state.dog)
+  console.log(props.location.state.dog, 'name')
+  const [dogImgs, setDogImgs] = useState('');
+
+
+  const fetchImg = () => {
+    fetch(`https://dog.ceo/api/breed/${props.location.state.dog}/images/random/4`)
+      .then(res => res.json())
+      .then(data => {
+        let arrs = Object.values(data);
+        let imgs = Object.values(arrs[0]);
+        console.log(imgs[0], 'data');
+        setDogImgs(imgs.map((img, idx) => {
+          return (
+            <div key={idx}>
+              <img src={img} alt="dog"></img>
+            </div>
+          )
+        }))
+      })
+  }
 
   return (
     <div >
 
       <div className='list'>
-        {props.location.state.dog ? props.location.state.dog: ''}
-        <div>dog1</div>
-        <div>dog2</div>
-        <div>dog3</div>
-        <div>dog4</div>
+        {props.location.state.dog ? props.location.state.dog : ''}
+        {dogImgs ? dogImgs : fetchImg()}
       </div>
-      
+
       <Link to='/'><button>Back</button></Link>
 
     </div>
