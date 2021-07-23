8463809eb47c29c95c42b156ade3a8fd4fb5dd95
satomi <satomiampuku2@gmail.com>
2021-07-22 18:44:33 -0700

    fix search

--- dog-search/component/home.jsx
+++ dog-search/component/home.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link, Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Result } from './result_page';
 
 export const Home = () => {
@@ -10,17 +10,70 @@ export const Home = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     setBreed('', breed);
+  
   }
 
-  const listOfDogs = () => {
+
+  const fetchDogList = () => {
     let ans = fetch('https://dog.ceo/api/breeds/list/all')
       .then(res => res.json())
       .then(data => {
         let obj = Object.values(data)
         let dogs = Object.keys(obj[0])
-        setDogs(dogs.map((dog, idx) => {
+        setDogs(dogs);
+      })
+  }
+  const listOfDogs = () => {
+    
+    !dogs ? fetchDogList() : 
+      setDogs(
+        dogs.filter((val) => {
+          if (val.toLowerCase().includes(breed.toLowerCase())) {
+            // console.log(val)
+            return val
+          }
+          
+        })
+        // dogs.map((dog, idx) => {
+          
+        //   return (
+        //     <p className='each-breed' key={idx}>
+              
+        //       <Link to={{
+        //         pathname: '/result',
+        //         state: { dog: dog }
+        //       }} >
+        //         {dog}
+        //       </Link>
+        //     </p>
+        //   )
+        // })
+      )
+  }
+
+
+ breed.length <=0 ? fetchDogList() : ''
+  return (
+    <div className='body'>
+
+      <form className='search-bar' onSubmit={handleSubmit}>
+        <label>Search Dogs </label>
+        <input type="text" placeholder='dog breed'
+          value={breed}
+          onChange={(e) => { setBreed(e.target.value); listOfDogs()}}
+        />
+        <Link to='/result'><button className='search-button'>WOFF</button> </Link>
+      </form>
+
+      <div className="list-container">
+        <div>List Of Dogs</div>
+        <div className='all-dogs'>
+          {
+           dogs ?  dogs.map((dog, idx) => {
+          
           return (
             <p className='each-breed' key={idx}>
+              
               <Link to={{
                 pathname: '/result',
                 state: { dog: dog }
@@ -29,26 +82,8 @@ export const Home = () => {
               </Link>
             </p>
           )
-        }));
-      })
-  }
-
-  return (
-    <div className='body'>
-
-      <form className='search-bar' onSubmit={handleSubmit}>
-        <label>Search Dogs </label>
-        <input type="text" placeholder='dog breed'
-          value={breed}
-          onChange={(e) => { setBreed(e.target.value) }}
-        />
-        <Link to='/result'><button className='search-button'>WOFF</button> </Link>
-      </form>
-
-      <div className="list-container">
-        <div>List Of Dogs</div>
-        <div className='all-dogs'>
-          {dogs ? dogs : listOfDogs()}
+        }) : 'No dogs yet'
+          }
         </div>
       </div>
 
